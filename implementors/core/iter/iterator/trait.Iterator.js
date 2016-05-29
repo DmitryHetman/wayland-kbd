9249668c1dfca13e2c682c8af661938761239129
Victor Berger <victor.berger@m4x.org>
2016-05-29 12:01:41 +0200

    Update docs for a5486f6ca7ae5d9c3bb510e98ce6d51fa1cb079e
    v0.4.0: update to wayland-client-0.6

---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wayland_kbd"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='wayland_kbd/struct.MappedKeyboard.html' title='wayland_kbd::MappedKeyboard'>MappedKeyboard</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
